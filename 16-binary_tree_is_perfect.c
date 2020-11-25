0ac636e16aff1f2817ec6a128d2fe606a0a55f72
aarizat <aarizatr@gmail.com>
2020-11-25 23:57:41 +0000

    Add functions

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,6 +11,39 @@ int _pow(int base, int power)
 	return (!power ? 1 : base * _pow(base, power - 1));
 }
 /**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ *
+ * Return: If tree is NULL, the function must return 0
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		return (binary_tree_size(tree->left) +
+			binary_tree_size(tree->right) + 1);
+	}
+	return (0);
+}
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: height of a binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t R = 0, L = 0;
+
+		L = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+		R = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+		return ((L > R) ? L : R);
+	}
+	return (0);
+}
+/**
  * binary_tree_is_perfect -  checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  *
